9f9689244b3d6f17474118ad6d071f431f71112d
Anand <anandejju@gmail.com>
2019-04-01 10:39:16 +0530

    Changes by Swarna second
    Signed-off-by: 'Anand <'anandejju@gmail.com'>

--- MyTestJavaProg.java
+++ MyTestJavaProg.java
@@ -1,6 +1,6 @@
 public class MyTestJavaProg{
 	public static void main(String[] args)
 	{
-		System.out.println("Test Program to Show Swarna");
+		System.out.println("Test Program to Show Swarnarrrrrrr");
 	}
 }
