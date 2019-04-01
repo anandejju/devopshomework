d00928755f2a78b23dc164fb55fbdfe0a8f148bd
Anand Ejju <anandejju@gmail.com>
2019-04-01 10:43:11 +0530

    changeString method added

--- MyTestJavaProg.java
+++ MyTestJavaProg.java
@@ -2,5 +2,10 @@ public class MyTestJavaProg{
 	public static void main(String[] args)
 	{
 		System.out.println("Test Program to Show Swarna");
+		string name ="Anand";
+		System.out.println(name);
+	}
+	public function changeString(string name){
+		return "name changed"+name;
 	}
 }
